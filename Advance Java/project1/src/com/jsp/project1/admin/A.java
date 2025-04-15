8c8d2f9c6f780a05868d88f5e4eb982add5e7793
Pawan Kumar <67819742+PawanPrince@users.noreply.github.com>
2025-04-15 13:42:28 +0100

    Add files via upload

---
+++ Advance Java/project1/src/com/jsp/project1/admin/A.java
@@ -0,0 +1,14 @@
+package com.jsp.project1.admin;
+
+/**
+ * @author Pawan 
+ * @since  04/10/25
+ */
+
+public class A {
+	
+	public static int addition(int a,int b ) {
+		return a+b;
+	}
+
+}
---
+++ Advance Java/project2/src/com/jsp/project2/user/B.java
@@ -0,0 +1,19 @@
+package com.jsp.project2.user;
+
+import com.jsp.project1.admin.A;
+
+/**
+ * @author Pawan Jha
+ * @since  04/10/25
+ */
+
+public class B {
+
+	public static void main(String[] args) {
+	
+		int sum = A.addition(23, 50);
+		System.out.println(sum);
+
+	}
+
+}
